f15290dad7325fbd36b5e21ade8b5415144e8f56
developer119 <developer119korea@gmail.com>
2018-03-25 17:28:51 +0900

    라우트속의 라우트를 사용했지만 작동이 안된다

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { Home, About } from 'pages';
-import Switch from 'react-router-dom/Switch';
+import { Route, Switch } from 'react-router-dom';
+import { Home, About, Posts } from 'pages';
+import Menu from 'components/Menu';
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Menu/>
                 <Route exact path="/" component={Home}/>
                 <Switch>
                     <Route path="/about/:name" component={About}/>
                     <Route path="/about" component={About}/>
                 </Switch>
+                <Route path="/posts" component={Posts}/>
             </div>
         );
     }
