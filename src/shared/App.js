7f18e4e3d8cec6f037642c234aa362609ae63502
developer119 <developer119korea@gmail.com>
2018-03-25 14:40:04 +0900

    params 를 사용하여 라우터 읽기

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { Home, About } from 'pages';
+import Switch from 'react-router-dom/Switch';
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Route exact path="/" component={Home}/>
-                <Route path="/about" component={About}/>
+                <Switch>
+                    <Route path="/about/:name" component={About}/>
+                    <Route path="/about" component={About}/>
+                </Switch>
             </div>
         );
     }
